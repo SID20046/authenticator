d9c45a29345269b29310f1bce758a98e36c96df4
SID20046 <167891162+SID20046@users.noreply.github.com>
2025-08-02 16:19:58 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ connectDB();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://authenticator2-theta.vercel.app/",
+  "https://authenticator-lemon.vercel.app/",
 ];
 
 app.use(express.json());
