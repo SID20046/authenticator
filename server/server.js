a84b535fc002c1d3ebedaee9fc207735d36dca50
SID20046 <sid20046@gmail.com>
2025-08-02 16:01:53 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ connectDB();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://mern-auth-client-seven.vercel.app",
+  "https://authenticator2-theta.vercel.app/",
 ];
 
 app.use(express.json());
